5f97aefced2ee70d7e29bab6d33cf74d14a3c80b
david benge <dbenge@adobe.com>
2017-07-14 20:09:24 -0700

    EOD checkin 2

---
+++ ${npm_package_name}-${npm_package_version}.js
@@ -0,0 +1,4 @@
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"main-action":[function(require,module,exports){
+'use strict';var request=require('request-promise'),documentdb=require('documentdb');function logData(a){request({method:'post',body:a,json:!0,url:'https://requestb.in/u0g37mu0'},function(c){c||console.log(body)})}function myEntrypoint(a){/* respond to the challenge request with an echo */return a.challenge?{challenge:a.challenge}:a.echo?{echo:a.echo}:void logData(a)}exports.default=myEntrypoint;
+
+},{"documentdb":undefined,"request-promise":undefined}]},{},[]);
