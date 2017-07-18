da9664798b2cb24844430da1e606ede33293e54b
david benge <dbenge@adobe.com>
2017-07-17 17:32:20 -0700

    working on issue with file name and dots

---
+++ io-event-logging-test.js
@@ -0,0 +1,32 @@
+var request = require('request-promise');
+//var documentdb = require('documentdb');
+
+function logData(postData) {
+  var options = {
+    method: 'post',
+    body: postData,
+    json: true,
+    url: 'https://requestb.in/15rsccl1'
+  }
+
+  request(options, function (error, response, data) {
+    if (!error) {
+      console.log(body);
+    }
+  });
+}
+
+function main(params) {
+  /* respond to the challenge request with an echo */
+  if (params.challenge) {
+    return { "challenge": params.challenge };
+  }
+
+  if(params.echo){
+    return { "echo": params.echo };
+  }
+
+  logData(params);
+}
+
+exports.main = main;
